2081627895729c5fec420eba7f24feb115e9b20c
David4500 <davidmenegasso82@gmail.com>
2020-06-20 22:00:03 -0300

    Commit Detetive virtual

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public edtDist;
+  public edtValor;
+  public cbContatos;
+  public rbTrabalho;
+  public rbLocal;
+  public resultado;
+
+  constructor() { }
+
+  onVerificarClick() {
+    let positivos = 0;
+
+    if (Number(this.edtDist) < 5) {
+      positivos++;
+    }
+    if (Number(this.edtValor) > 1000) {
+      positivos++;
+    }
+    if (this.cbContatos == "mais5") {
+      positivos++;
+    }
+    if (this.rbTrabalho == "sim") {
+      positivos++;
+    }
+    if (this.rbLocal == "sim") {
+      positivos++;
+    }
+    this.resultado = "Você foi considerado ";
+
+    if (positivos < 2) {
+      this.resultado += "Inocente";
+    } else if (positivos < 3) {
+      this.resultado += "Suspeito";
+    } else if (positivos < 5) {
+      this.resultado += "Cúmplice";
+    } else {
+      this.resultado += "Assassino";
+    }
+    this.resultado += "!";
+
+
+  }
 
 }
